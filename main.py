4f58cd2de3e74ac535e3c2bd95523174cb788172
Isaac Watts <Isaac.s.watts@gmail.com>
2022-11-28 01:44:39 -0500

    Calc 2020-2021 mean

---
+++ main.py
@@ -0,0 +1,16 @@
+file = open("C:/Users/jwang/Documents/Erika_Project/Project-1/Attendance.txt") #open the text file
+lines = file.readlines() #lines is now a list []
+file.close() #close file
+
+pq = [] 
+
+for x in range(len(lines)):
+    if(x==0): #skips over the labels using continue
+        continue
+    pq.append(lines[x].split(",")) #appends each list to 
+
+sum = 0 #init sum
+for i in range(len(pq)): #len(pq)==1081 so it will loop from 0 to 1081 for indexing
+    sum += float(pq[i][5]) #using list splicing ex: li1 = [[0,1,2],[4,5,6]] li1[0][1] == 1 convert to float bc String
+
+print(sum/len(pq)) #gives me the mean value
