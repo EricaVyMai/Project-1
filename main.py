e6aa537eb42feee4299253a3d19114ebfc67c876
IsaacVSU <113640599+IsaacVSU@users.noreply.github.com>
2022-11-30 02:23:43 -0500

    Mean of all the school districts now
    Made it so that the mean doesn't just take it from all of them, but specific school districts.

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ file = open("Attendance.txt","r") #open the text file
 lines = file.readlines() #lines is now a list []
 file.close() #close file
 
-pq = [] 
+pq = []
 
-for x in range(len(lines)):
-    if(x==0): #skips over the labels using continue
-        continue
+#Hashmap to evaluate occurence of school districts
+HM = {
+
+}
+#Hashmap to for the total of rates in 2020-2021
+SumHM = {
+
+}
+
+for x in range(1,len(lines)):
     pq.append(lines[x].split(",")) #appends each list to 
 
-sum = 0 #init sum
 for i in range(len(pq)): #len(pq)==1081 so it will loop from 0 to 1081 for indexing
-    sum += float(pq[i][5]) #using list splicing ex: li1 = [[0,1,2],[4,5,6]] li1[0][1] == 1 convert to float bc String
+    HM[pq[i][1]] = 0 #Will get every single school district in pq
+    SumHM[pq[i][1]] = 0
 
-print(sum/len(pq)) #gives me the mean value
+for i in range(len(pq)):
+    if(pq[i][1] in HM.keys()): #check for if school is alr in HM (ALWAYS IS)
+        HM[pq[i][1]]+=1
+    if(pq[i][1] in SumHM.keys()): #Ditto
+        SumHM[pq[i][1]]+= float(pq[i][5])
+
+for key, value in HM.items(): #Evaluates For all the keys, the mean for every single School district
+    print(f"{key} : {SumHM[key]/value}") 
+print(len(HM)) #197 schools
